1ff40f8a44e58382cb4ca2fc6d762a90ef5c0d1a
Sagar Sharma <123538728+Amansagar1@users.noreply.github.com>
2025-03-09 16:17:31 +0530

    changes

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+
+const app = express();
+
+// Middleware
+app.use(cors()); 
+app.use(express.json()); 
+
+// Routes
+app.use('/api/auth', authRoutes); 
+app.use('/api/tasks', taskRoutes); 
+
+// Database connection
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
