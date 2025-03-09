d898f2daf0dd6f2c04d4c9408ffb130b73ad539f
Sagar Sharma <123538728+Amansagar1@users.noreply.github.com>
2025-03-09 15:50:16 +0530

    name change

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const cors = require('cors');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
-
+const https = require('https');
 
 const app = express();
 
 // Middleware
 app.use(cors()); 
 app.use(express.json()); 
-
+app.use(cors({
+  origin: 'https://hotelsudarshan.com', 
+}));
 // Routes
 app.use('/auth', authRoutes); 
 app.use('/tasks', taskRoutes); 
@@ -24,6 +26,20 @@ mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-// Start server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Basic route to check server status
+app.get('/', (req, res) => {
+  res.send('task management API is running...');
+});
+// Start the server// HTTPS Server Setup
+const options = {
+// cert: fs.readFileSync('./certificates/fullchain.pem'),
+// key: fs.readFileSync('./certificates/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/hotelsudarshan.com/fullchain.pem'),
+key: fs.readFileSync('/etc/letsencrypt/live/hotelsudarshan.com/privkey.pem'),
+};
+
+// Create an HTTPS server
+https.createServer(options, app).listen(8000, () => {
+console.log(`Server running on  ${PORT}`);
+});
 
